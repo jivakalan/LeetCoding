995021f9b2734cc0f4e7bfaf5a4eeb72b6db66ae
Jiva <kalan.jiva@gmail.com>
2020-10-17 18:04:44 -0400

    containsDuplicateII final

--- containsDuplicateII.py
+++ containsDuplicateII.py
@@ -57,18 +57,68 @@ a.containsNearbyDuplicate2(nums = [1,0,1,1], k = 1)
 
 ######################time limit exceeded on above solution
 
+#try creating hash table 
+
+# =============================================================================
+# Given an array of integers and an integer k, find out whether there are two 
+# distinct indices i and j in the array such that nums[i] = nums[j] and the absolute 
+# difference between i and j is at most k.
+
+# Example 1:
+# 
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
+# Input: nums = [1,0,1,1], k = 1
+# Output: true
+
+# Example 3:
+# 
+# Input: nums = [1,2,3,1,2,3], k = 2
+# Output: false
+# =============================================================================
 
 
 class Solution(object):
-    def containsNearbyDuplicate(self,nums,k):
+    def containsNearbyDuplicate(self,nums,k):        
                 
-                for i in range(0,len(nums)):
-                    for j in range(0,len(nums)):
-                        print(i,j,nums[i]==nums[j],i!=j,abs(i-j)<=k)
-                      
-                        if nums[i]==nums[j] and i!=j and abs(i-j)<=k:
-                            return True 
-                        
-                return False
+        mydict={}           
+        for i in range(0,len(nums)):
+            if nums[i] in mydict and i-mydict[nums[i]]<=k:
+                return True
+                break
+            else:
+                mydict[nums[i]]= i
+        return False
+    
 a=Solution()                
-a.containsNearbyDuplicate(nums = [1,0,1,1], k = 1)
+
+a.containsNearbyDuplicate( [1,2,3,1,2,3], k = 2)
+
+nums=[1,0,1,1]
+k = 1
+    
+mydict={}           
+for i in range(0,len(nums)):
+    if nums[i] in mydict and i-mydict[nums[i]] <= k:
+        print('True')
+    else:
+        mydict[nums[i]]= i
+   
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
