64f9b264447d7d9a470c4d1c10242a89f6cbfbe3
unknown <kalan.jiva@gmail.com>
2020-11-11 23:57:41 -0500

    findwords

---
+++ findWords.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 23:55:50 2020
+
+@author: kalan
+"""
+# =============================================================================
+# 
+# 500. Keyboard Row
+# Easy
+# 
+# Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+# 
+# =============================================================================
+ 
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        
