c825bdfc6bf74031da8941d513fa3f136515e4cd
unknown <kalan.jiva@gmail.com>
2020-11-24 23:09:06 -0500

    nov24

---
+++ mergeTrees.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 18:43:52 2020
+
+@author: kalan
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        
+        
+## how do you merge two trees
+each node...value ...sum up...
+i guess...create a neww tree --
