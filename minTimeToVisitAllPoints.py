020ed2925a5ddf17b17b75efbe5c1c24b8af0dd7
unknown <kalan.jiva@gmail.com>
2020-12-15 22:00:08 -0500

    dec15-2

--- minTimeToVisitAllPoints.py
+++ minTimeToVisitAllPoints.py
@@ -31,9 +31,12 @@ Created on Tue Dec  8 20:01:49 2020
 # Output: 5
 # =============================================================================
 
-points = [[1,1],[3,4],[-1,0]]
 
 points = [[3,2],[-2,2]]
+points = [[1,1],[3,4],[-1,0]]
+ 
+points = [[431,91],[837,-127]]
+
 
 cnt=0
 for i in range(0,len(points)):
@@ -41,66 +44,89 @@ for i in range(0,len(points)):
         
         init = points[i]
         dest = points[i+1]
-        
-        
-        while dest[0]-init[0] < dest[1]-init[1]:
-               
-            #if x < y then move vertically
-            #move vertically
-            init[1] =  init[1]+1
-            cnt+=1
-            #have to move verticaly or horizontally
-        while dest[0]-init[0] > dest[1]-init[1]:
-            init[0] =  init[0]+1
-            cnt+=1
-        
-        while init != dest:
+  
+        while  dest[0]-init[0] != dest[1]-init[1]:
+            if abs(dest[1]-init[1]) > abs(dest[0]-init[0]) :
+                if dest[1] < init[1]:
+                    init[1]=init[1]-1
+                    cnt+=1    
+                else:
+                    init[1]=init[1]+1
+                    cnt+=1
+                    print(init)
+                
+                
+            else:
+                if dest[0]<init[0]:
+                    
+                    init[0]=init[0]-1
+                    cnt+=1
+                if dest[0]>init[0]:
+                    init[0]=init[0]+1
+                    cnt+=1
+        while init != dest :
+            
             if dest[0]<0:
                 init = [x-1 for x in init]
             else:
                 init = [x+1 for x in init]
             cnt+=1
-
-
-
-
-
+            
+            
+            
+            
 
 
 
     
 class Solution:
     def minTimeToVisitAllPoints(self, points):
-        
-        
+        #
         cnt=0
+
         for i in range(0,len(points)):
             if i+1 < len(points):
                 
                 init = points[i]
                 dest = points[i+1]
-        
-        
-                while dest[0]-init[0] < dest[1]-init[1]:
-                       
-                    #if x < y then move vertically
-                    #move vertically
-                    init[1] =  init[1]+1
-                    cnt+=1
-                    #have to move verticaly or horizontally
-                while dest[0]-init[0] > dest[1]-init[1]:
-                    init[0] =  init[0]+1
-                    cnt+=1
                 
-                while init != dest:
+                
+                
+                
+                while  dest[0]-init[0] != dest[1]-init[1]:
+                    if abs(dest[1]-init[1]) > abs(dest[0]-init[0]) :
+                        if dest[1] < init[1]:
+                            init[1]=init[1]-1
+                            cnt+=1    
+                        else:
+                            init[1]=init[1]+1
+                            cnt+=1
+                        
+                        
+                    else:
+                        if dest[0]<init[0]:
+                            
+                            init[0]=init[0]-1
+                            cnt+=1
+                        if dest[0]>init[0]:
+                            init[0]=init[0]+1
+                            cnt+=1
+                while init != dest :
+                    
                     if dest[0]<0:
                         init = [x-1 for x in init]
                     else:
                         init = [x+1 for x in init]
                     cnt+=1
+            
         return cnt
-
-
 
 a=Solution()
 a.minTimeToVisitAllPoints(points)
+
+
+
+
+#time limit exceeded 
+--
+points = [[559,511],[932,618],[-623,-443],[431,91],[838,-127],[773,-917],[-500,-910],[830,-417],[-870,73],[-864,-600],[450,535],[-479,-370],[856,573],[-549,369],[529,-462],[-839,-856],[-515,-447],[652,197],[-83,345],[-69,423],[310,-737],[78,-201],[443,958],[-311,988],[-477,30],[-376,-153],[-272,451],[322,-125],[-114,-214],[495,33],[371,-533],[-393,-224],[-405,-633],[-693,297],[504,210],[-427,-231],[315,27],[991,322],[811,-746],[252,373],[-737,-867],[-137,130],[507,380],[100,-638],[-296,700],[341,671],[-944,982],[937,-440],[40,-929],[-334,60],[-722,-92],[-35,-852],[25,-495],[185,671],[149,-452]]
