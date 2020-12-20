f899969eb34b91eeeff3925d32f78a2aec80d783
Jiva <kalan.jiva@gmail.com>
2020-12-19 19:19:28 -0500

    dec19-3

--- numberOfMatches.py
+++ numberOfMatches.py
@@ -5,36 +5,49 @@ Created on Sat Dec 19 19:06:20 2020
 @author: jkalan
 """
 
-1688. Count of Matches in Tournament
-You are given an integer n, the number of teams in a tournament that has strange rules:
+# =============================================================================
+# 1688. Count of Matches in Tournament
+# You are given an integer n, the number of teams in a tournament that has strange rules:
+# 
+# If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+# If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+# Return the number of matches played in the tournament until a winner is decided.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: n = 7
+# Output: 6
+# Explanation: Details of the tournament: 
+# - 1st Round: Teams = 7, Matches = 3, and 4 teams advance.
+# - 2nd Round: Teams = 4, Matches = 2, and 2 teams advance.
+# - 3rd Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
+# Total number of matches = 3 + 2 + 1 = 6.
+# =============================================================================
 
-If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
-If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
-Return the number of matches played in the tournament until a winner is decided.
 
- 
-
-Example 1:
-
-Input: n = 7
-Output: 6
-Explanation: Details of the tournament: 
-- 1st Round: Teams = 7, Matches = 3, and 4 teams advance.
-- 2nd Round: Teams = 4, Matches = 2, and 2 teams advance.
-- 3rd Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
-Total number of matches = 3 + 2 + 1 = 6.
-
-
-7:06
-
-n=7
-
+7:09
+7:19
 
 
 class Solution(object):
     def numberOfMatches(self, n):
+        
         """
         :type n: int
         :rtype: int
         """
-        
+        advance =0
+        out= 0
+        while n>1:
+                
+            if n%2 == 0:
+                advance = n/2
+                out+=n/2
+            else:
+                advance = (n-1)/2 + 1
+                out+=(n-1)/2
+            n = advance
+            
+            print(out)
