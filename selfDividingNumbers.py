1011457cc036aa4f45ac09ed3c3147dc9a6899ec
unknown <kalan.jiva@gmail.com>
2020-12-21 00:14:42 -0500

    'dec-21'

--- selfDividingNumbers.py
+++ selfDividingNumbers.py
@@ -4,23 +4,42 @@ Created on Fri Dec 18 23:40:10 2020
 
 @author: kalan
 """
-
-728. Self Dividing Numbers
-
-A self-dividing number is a number that is divisible by every digit it contains.
-
-For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
-
-Also, a self-dividing number is not allowed to contain the digit zero.
-
-Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
-
-Example 1:
+# =============================================================================
+# 
+# A self-dividing number is a number that is divisible by every digit it contains.
+# 
+# For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+# 
+# Also, a self-dividing number is not allowed to contain the digit zero.
+# 
+# Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+# 
+# =============================================================================
+# =============================================================================
+# Example 1:
+# =============================================================================
 Input: 
-left = 1, right = 22
-Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+left = 1 
+right = 22
+Output: [66,708]
 
 
 class Solution:
-    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+    def selfDividingNumbers(self, left,right):
         
+        out=[]
+        for number in range(left,right+1):
+
+                cnt=0                    
+                for digit in str(number):
+                    if int(digit)==0:
+                        break
+                    if number % int(digit) == 0:
+                        cnt+=1
+                        #print(number,cnt)
+                    
+                    if cnt == len(str(number)):
+                          
+                        out.append(number)
+        return out 
+    
