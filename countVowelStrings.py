23f41c9907c7c2266cef268d6db0aebb8f271cb4
unknown <kalan.jiva@gmail.com>
2021-01-09 13:47:13 -0500

    jan9-3

--- countVowelStrings.py
+++ countVowelStrings.py
@@ -4,6 +4,8 @@ Created on Sat Jan  9 13:28:53 2021
 
 @author: kalan
 """
+1:31
+
 
 1641. Count Sorted Vowel Strings
 Medium
@@ -13,6 +15,7 @@ Given an integer n, return the number of strings of length n that consist only o
 A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
 
  
+
 
 Example 1:
 
@@ -26,7 +29,7 @@ Output: 15
 Explanation: The 15 sorted strings that consist of vowels only are
 ["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"].
 Note that "ea" is not a valid string since 'e' comes after 'a' in the alphabet.
-Example 3:
+
 
 Input: n = 33
 Output: 66045
@@ -35,6 +38,33 @@ Constraints:
 
 1 <= n <= 50
 
+#add repeats (aaa, aaa,aae)
+    
+#create all 3 letter combinatoins 
+
+
+
+
+
+
 class Solution:
-    def countVowelStrings(self, n: int) -> int:
+    def countVowelStrings(self, n):
         
+        
+        from itertools import permutations
+        
+        #for n = 3
+        vowels =['a','e','i','o','u']
+        
+        perm = permutations(vowels,2)
+        out=[]
+        for i in list(perm):
+            #print(sorted(i) == list(i))
+            if sorted(i) == list(i):
+                
+                out.append(i)
+        
+        for i in vowels:
+            out.append(i+i)
+            
+        return len(out)
