29ea7cd9f806ca63ea514a81c8aa9e682d76e638
unknown <kalan.jiva@gmail.com>
2021-01-15 18:15:38 -0500

    'jan15

--- searchBST.py
+++ searchBST.py
@@ -11,18 +11,63 @@ Created on Wed Jan 13 23:18:22 2021
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
-        
 
+
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.val = data
+
+# Insert method to create nodes
+    def insert(self, data):
+
+        if self.val:
+            if data < self.val:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.val:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.val = data
+
+# Print the tree
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.val),
+        if self.right:
+            self.right.PrintTree()
+
+
+root = Node(4)
+root.insert(2)
+root.insert(7)
+root.insert(1)
+root.insert(3)
+
+root.val
+
+class Solution:
+    def searchBST(self, root,val):
         
-        if root.node == val:
+        if root.val == val:
             return root
         
-        if root.node > val:
+        if root.val > val:
             self.searchBST(root.left, val)
         
-        if root.node < val:
+        if root.val < val:
             self.searchBST(root.right, val)
+            
         
-        
+a=Solution()
+val=2
+a.searchBST(root, val)        
