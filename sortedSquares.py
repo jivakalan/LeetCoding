4f51001a6090d99993a94523bb04d591957bfd52
unknown <kalan.jiva@gmail.com>
2021-01-22 20:14:19 -0500

    jan22-2

--- sortedSquares.py
+++ sortedSquares.py
@@ -22,6 +22,25 @@ Explanation: After squaring, the array becomes [16,1,0,9,100].
 After sorting, it becomes [0,1,9,16,100].
 
 
+
+nums = [-4,-1,0,3,10]
+
+
+
+
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
+    def sortedSquares(self, nums):
         
+
+        out=[]
+        for i in nums:
+            out.append(i*i)
+        out.sort()
+        return out
+
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+
+a=Solution()
+a.sortedSquares(nums = [-7,-3,2,3,11])
