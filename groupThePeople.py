a139d1de001c4481d331e274ac4a72be296bd7b4
unknown <kalan.jiva@gmail.com>
2021-02-10 22:52:31 -0500

    feb10

--- groupThePeople.py
+++ groupThePeople.py
@@ -1,22 +1,24 @@
 
-1282. Group the People Given the Group Size They Belong To
-Medium
-
-There are n people that are split into some unknown number of groups. Each person is labeled with a unique ID from 0 to n - 1.
-
-You are given an integer array groupSizes, where groupSizes[i] is the size of the group that person i is in. For example, if groupSizes[1] = 3, then person 1 must be in a group of size 3.
-
-Return a list of groups such that each person i is in a group of size groupSizes[i].
-
-Each person should appear in exactly one group, and every person must be in a group. If there are multiple answers, return any of them. It is guaranteed that there will be at least one valid solution for the given input.
-
- 
-groupSizes[1]=3 ---person 1 is in a group of size 3
-
-
-reutn a list of groups that each perosn i is in a group of s
-
-
+# =============================================================================
+# 1282. Group the People Given the Group Size They Belong To
+# Medium
+# 
+# There are n people that are split into some unknown number of groups. Each person is labeled with a unique ID from 0 to n - 1.
+# 
+# You are given an integer array groupSizes, where groupSizes[i] is the size of the group that person i is in. For example, if groupSizes[1] = 3, then person 1 must be in a group of size 3.
+# 
+# Return a list of groups such that each person i is in a group of size groupSizes[i].
+# 
+# Each person should appear in exactly one group, and every person must be in a group. If there are multiple answers, return any of them. It is guaranteed that there will be at least one valid solution for the given input.
+# 
+#  
+# groupSizes[1]=3 ---person 1 is in a group of size 3
+# 
+# 
+# reutn a list of groups that each perosn i is in a group of s
+# 
+# 
+# =============================================================================
 Example 1:
 
 Input: groupSizes = [3,3,3,3,3,1,3]
@@ -32,15 +34,13 @@ Input: groupSizes = [2,1,3,3,3,2]
 Output: [[1],[0,5],[2,3,4]]
  
 
-Constraints:
-
-groupSizes.length == n
-1 <= n <= 500
-1 <= groupSizes[i] <= n
-
 
 groupSizes = [3,3,3,3,3,1,3]
 
+for i in range(len(groupSizes)):
+    print('person', i+1, 'is in a group of size', groupSizes[i])
+    
+go
 out = []
 
 
