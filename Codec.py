772205c92b61a6d7b63f59e3d5972d628ca6b83f
unknown <kalan.jiva@gmail.com>
2021-03-23 21:50:16 -0400

    mar23

--- Codec.py
+++ Codec.py
@@ -5,13 +5,15 @@ Created on Mon Mar 22 22:54:18 2021
 @author: kalan
 """
 
-535. Encode and Decode TinyURL
-Medium
-
-Note: This is a companion problem to the System Design problem: Design TinyURL.
-TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
-
-Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+# =============================================================================
+# 535. Encode and Decode TinyURL
+# Medium
+# 
+# Note: This is a companion problem to the System Design problem: Design TinyURL.
+# TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+# 
+# Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+# =============================================================================
 
 
 class Codec:
