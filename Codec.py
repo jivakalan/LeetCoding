329dd93e39cedc487da46cb628d2a70ebe5272bb
unknown <kalan.jiva@gmail.com>
2021-03-27 00:07:35 -0400

    mar27

--- Codec.py
+++ Codec.py
@@ -14,50 +14,34 @@ Created on Mon Mar 22 22:54:18 2021
 # 
 # Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
 # =============================================================================
-
+import random
 
 class Codec:
-    import random
+    def __init__(self):
+        self.dic={}
+        self.dic2={}
+        self.dist = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        self.shortUrl = ''
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        self.dic={}
-        dist = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
         #if longUrl not in self.dic:
-        for key,value in self.dic.items():
-            if longUrl not in value:
-                
-                shortUrl=''.join(random.choice(dist) for i in range(6))
-                self.dic[shortUrl]= longUrl
-        
-#        return 'http://tinyurl.com/'+shortUrl
+        shortUrl=''.join(random.choice(self.dist) for i in range(6))
+        if shortUrl not in self.dic:
+            self.dic[shortUrl]= longUrl
+            self.dic2[longUrl]= shortUrl
+            self.shortUrl = shortUrl
+        return 'http://tinyurl.com/'+shortUrl
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
-        """
-        
-        return self.dic[shortUrl]
+        """        
+        return self.dic[self.shortUrl]
 
 longUrl='https://leetcode.com/problems/design-tinyurl'
 url='https://leetcode.com/problems/design-tinyurl'
 
 
 codec = Codec()
-
-codec.encode(url)
-
-codec.decode('81DoQR')
-
 codec.decode(codec.encode(url))
-
-
-dic={}
-dist = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-
-#if longUrl not in self.dic:
-for key,value in dic.items():
-    if:
-        
-        shortUrl=''.join(random.choice(dist) for i in range(6))
-        dic[shortUrl]= longUrl
---
+++ sumEvenGrandparent.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 19:55:09 2021
+
+@author: kalan
+"""
+
+1315. Sum of Nodes with Even-Valued Grandparent
+Medium
+
+Given a binary tree, return the sum of values of nodes with even-valued grandparent.  (A grandparent of a node is the parent of its parent, if it exists.)
+
+If there are no nodes with an even-valued grandparent, return 0.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 18
+Explanation: The red nodes are the nodes with even-value grandparent while the blue nodes are the even-value grandparents.
+ 
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        it s
