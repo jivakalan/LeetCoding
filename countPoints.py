e80eb976347f77fb5678b96f1f734e316e1e5bc4
unknown <kalan.jiva@gmail.com>
2021-04-22 18:46:44 -0400

    'apr22'

--- countPoints.py
+++ countPoints.py
@@ -26,7 +26,34 @@ Created on Tue Apr 20 19:22:35 2021
 # Explanation: The points and circles are shown above.
 # queries[0] is the green circle, queries[1] is the red circle, and queries[2] is the blue circle.
 
+import math
 
 class Solution:
-    def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
+    def countPoints(self, points, queries):
         
+        
+        out = []
+        
+        for cc in queries:
+            cnt = 0
+            for p in points:
+             #   print(p)
+              #  print(p, cc)
+                # print((p[0]-cc[0])**2 ,(p[1]-cc[1])**2 , (cc[2])**2)
+                
+                #val = (p[0]-cc[0])**2 + (p[1]-cc[1])**2 - (cc[2])**2
+                val = math.sqrt((p[0]-cc[0])**2 + (p[1]-cc[1])**2)
+                
+                # print(val, cc)
+                
+                
+                if val <= cc[2]:
+                #if val==0:
+                    cnt+=1
+                    
+            out.append(cnt)
+        return out 
+    
+a=Solution()
+a.countPoints(points = [[1,3],[3,3],[5,3],[2,2]],queries = [[2,3,1],[4,3,1],[1,1,2]])
+a.countPoints(points = [[1,1],[2,2],[3,3],[4,4],[5,5]], queries = [[1,2,2],[2,2,2],[4,3,2],[4,3,3]])
