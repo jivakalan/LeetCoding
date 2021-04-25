d75cddf698ec039a04b947864568e02ac83dd61b
unknown <kalan.jiva@gmail.com>
2021-04-25 11:16:10 -0400

    apr25

--- constructMaximumBinaryTree.py
+++ constructMaximumBinaryTree.py
@@ -16,27 +16,48 @@ Created on Wed Apr 21 17:38:52 2021
 # Return the maximum binary tree built from nums
 
 
-nums = [3,2,1,6,0,5]
-
-root = TreeNode(max(nums))
-
-for i in range(0, nums.index(max(nums))):
-    l_arr = nums[0:nums.index(max(nums))]
-    r_arr = nums[nums.index(max(nums))+1:]
-    if l_arr:
-      #insert to left  
-        root.insert(max(l_arr))'
-    if r_arr:
-        #insert to right
-        root.insert(max(r_arr))
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+class TreeNode:
+  #initialize the attributes of the node
+  def __init__(self,data):
+      self.data = data
+      self.left = None
+      self.right = None  
+
 class Solution:
-    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+    def constructMaximumBinaryTree(self, nums):
         
+        def insert(root, data):
+                if root.left is None:
+                    root.left = TreeNode(data)
+                else:
+                    insert(root.left, data)
+        def r_insert(root, data):
+                if root.right is None:
+                    root.right = TreeNode(data)
+                else:
+                    r_insert(root.right, data)
+        
+        def maxtree(root, nums):
+            
+            l_arr = nums[0:nums.index(max(nums))]
+            r_arr = nums[nums.index(max(nums))+1:]
+                
+            
+            if l_arr: 
+                insert(root,max(l_arr))
+                maxtree(root.left, l_arr)
+                
+            if r_arr:
+                r_insert(root, max(r_arr))
+                maxtree(root.right, r_arr)
+        
+                
+        root = TreeNode(max(nums))
+        maxtree(root, nums)
+        return root   
+    
+a=Solution()
+root = a.constructMaximumBinaryTree(nums = [3,2,1,6,0,5])
